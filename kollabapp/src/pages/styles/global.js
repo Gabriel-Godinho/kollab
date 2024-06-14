961d3cd487ff7cd7be5a609811f414e39fe9af4f
Gabriel Godinho <gabriel234417@gmail.com>
2024-06-14 17:41:32 -0300

    Ajuste na tela

--- kollabapp/src/pages/styles/global.js
+++ kollabapp/src/pages/styles/global.js
@@ -9,8 +9,8 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    width: 100vw;
-    height: 100vh;
+    width: 100%;
+    height: 100%;
     background-color: #f0f2f5;
     font-family: "Gill Sans", sans-serif;
   }
